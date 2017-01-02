c27f62d992bfd2f53d546902e76a2a57222e5163
Mathew Thomas <mathew.thomas@longdivision.com>
2017-01-02 13:01:28 -0500

    Create Lab-3.rb

---
+++ DynamoDb/Lab-3.rb
@@ -0,0 +1,13 @@
+require 'aws-sdk'
+
+dynamoDB = Aws::DynamoDB::Resource.new(region: 'us-east-1')
+
+table = dynamoDB.table('mthomas-test-users')
+
+table.put_item({
+  item:
+    {
+      "ID" => 123456,
+      "FirstName" => 'Snoop',
+      "LastName" => 'Doug'
+}})
