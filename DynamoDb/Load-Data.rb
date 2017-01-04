3acbc40fac5852af7e3d93298f32d761d8879cf7
Mathew Thomas <mathew.thomas@longdivision.com>
2017-01-03 18:08:15 -0800

    Update Load-Data.rb

--- DynamoDb/Load-Data.rb
+++ DynamoDb/Load-Data.rb
@@ -22,3 +22,34 @@ table.put_item({
     }
   })
 
+table.put_item({
+  item:
+    {
+      'ID' => 123457,
+      'Title' => 'Book 202',
+      'ISBN' => '202-202222222',
+      'Authors' => 'Author - 2',
+      'Price' => 2,
+      'Dimensions' => '8.5 x 11.0 x 0.5',
+      'PageCount' => 200,
+      'InPublication' => 1,
+      'ProductCategory' => 'Book'
+    }
+  })
+
+
+table.put_item({
+  item:
+    {
+      'ID' => 123458,
+      'Title' => 'Book 303',
+      'ISBN' => '303-303333333',
+      'Authors' => 'Author - 3',
+      'Price' => 2,
+      'Dimensions' => '8.5 x 11.0 x 0.5',
+      'PageCount' => 500,
+      'InPublication' => 1,
+      'ProductCategory' => 'Book'
+    }
+  })
+
