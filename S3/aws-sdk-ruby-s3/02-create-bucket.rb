1dbc0ffef853c609a8a7cfdaaf23f563b9681ee3
Ubuntu <ubuntu@ip-10-0-1-92.ec2.internal>
2017-01-07 20:25:30 +0000

    check if bucket exists
    Signed-off-by: Ubuntu <ubuntu@ip-10-0-1-92.ec2.internal>

--- S3/aws-sdk-ruby-s3/02-create-bucket.rb
+++ S3/aws-sdk-ruby-s3/02-create-bucket.rb
@@ -9,6 +9,12 @@ puts "hello, world"
 region = 'us-east-1'
 s3 = Aws::S3::Resource.new(region: region)
 
-s3.create_bucket(bucket: 'mthomas-labs')
 
+if s3.bucket('mthomas-labs').exists?
+  puts "mthomas-labs bucket already exists."
+  exit
+end
+
+puts "creating bucket mthomas-labs"
+s3.create_bucket(bucket: 'mthomas-labs')
 
