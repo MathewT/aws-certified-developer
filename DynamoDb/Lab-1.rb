39bf78285edc52e95dc0413ece16baa85ff81b82
Mathew Thomas <mathew.thomas@longdivision.com>
2017-01-27 16:17:29 -0500

    lab update.
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- DynamoDb/Lab-1.rb
+++ DynamoDb/Lab-1.rb
@@ -1,8 +1,11 @@
 require 'aws-sdk'
 
-dynamoDB = Aws::DynamoDB::Resource.new(region: 'us-east-1')
+puts "hello world."
 
+dynamoDB = Aws::DynamoDB::Resource.new(region: 'us-east-1')
+      
 dynamoDB.tables.each do |t|
   puts "Name:    #{t.name}"
   puts "#Items:  #{t.item_count}"
 end
+
